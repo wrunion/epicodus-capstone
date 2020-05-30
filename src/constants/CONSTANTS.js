78192cd482b0c1c5bcf23a43497e87794f291820
wrunion <winterrunion@gmail.com>
2020-05-30 13:56:59 -0700

    Build out search terms.

--- src/constants/CONSTANTS.js
+++ src/constants/CONSTANTS.js
@@ -1,4 +1,7 @@
 export const c = {
   APP_TOKEN: "$$app_token=NDQ8EOLrXHft9YeGZ2axBbxzb",
-  BASE_URL: "https://data.cityofnewyork.us/resource/pqg4-dm6b.json"
+  BASE_URL: "https://data.cityofnewyork.us/resource/pqg4-dm6b.json",
+  ADDRESS: 'address1',
+  AGING: 'aging',
+  EDUCATION: 'education'
 }
