669667b8aaeef8ee396a1c14d8455379c544143f
wrunion <winterrunion@gmail.com>
2020-05-29 18:42:19 -0700

    save before microwave.

--- NOTES.md
+++ NOTES.md
@@ -4,6 +4,10 @@ my dataset: https://data.cityofnewyork.us/resource/pqg4-dm6b.json
 sample query:
 https://data.cityofnewyork.us/resource/pqg4-dm6b.json?bronx=Y&arts_culture=Y&housing=Y&disabilities=Y
 
+APP TOKENS: 
+"Once you have an app token, you can include it with your request either by using the X-App-Token HTTP header, or by passing it via the $$app_token parameter on your URL."
+
+My app token: "NDQ8EOLrXHft9YeGZ2axBbxzb"
 
 Usage notes from the docs at https://dev.socrata.com/foundry/data.cityofnewyork.us/pqg4-dm6b:
 $.ajax({
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import Axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.baseurl = "https://data.cityofnewyork.us/resource/pqg4-dm6b.json";
     this.appToken = "NDQ8EOLrXHft9YeGZ2axBbxzb"
+  }
+
+  onSearchSubmit(term) {
+    Axios.get(this.baseurl, {
+      params: { 
+        query: term, 
+        $$app_token: this.appToken
+      }
+    });
+  }
+
+  onSearch(term) {
+    Axios.get(`${this.baseurl}?${term}=Y&$$app_token=${this.appToken}`)
+  }
+  //OR: 
+  onSearchAlt(term) {
+    fetch('https://data.cityofnewyork.us/resource/pqg4-dm6b.json?bronx=Y&arts_culture=Y&housing=Y&disabilities=Y&$$app_token=NDQ8EOLrXHft9YeGZ2axBbxzb')
+      .then(response => response.json())
+      .then(data => console.log(data));
   }
 
   render(){
