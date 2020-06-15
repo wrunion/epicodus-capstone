472eeb71c9fee276057f01162ba18f5c966d3f5e
wrunion <winterrunion@gmail.com>
2020-06-15 13:58:32 -0700

    commit before branching.

--- src/components/KeywordSearch.js
+++ src/components/KeywordSearch.js
@@ -3,16 +3,7 @@ import { Input, Button, Form } from 'semantic-ui-react';
 import "./App.css";
 
 function KeywordSearch(props) {
-  // const { results, searchTerm } = props;
 
-  // const resultsWithDescriptions = results.filter(e => e.description);
-
-  // const lowercaseResults = results.map(e => {
-  //   return {...e, organizationname: e.organizationname.toLowerCase(), description: e.description.toLowerCase()}
-  // });
-
-
-  // const searchResults = (term, arr) => arr.filter(e => e.description.includes([term]) || e.organzationname.includes([term]));
   function handleSubmit(e) {
     e.preventDefault();
     props.callbackSubmissionHandler(e.target.term.value)
@@ -21,12 +12,18 @@ function KeywordSearch(props) {
   return (
       <div className="KeywordSearch ui segment">
         <Form onSubmit={handleSubmit}>
-          <h3>Keyword Search:</h3>
+          <h3>I am looking for:</h3>
           <Input 
             id="term"
             required
             icon='search' 
             placeholder='Search...' /> <br /><br />
+          <h3>Located in:</h3>
+          <Input 
+            id="term"
+            required
+            icon='search' 
+            placeholder='Search...' />
           <Button type="submit" className="green basic">Submit</Button>
         </Form>
       </div>
@@ -34,12 +31,3 @@ function KeywordSearch(props) {
 }
 
 export default KeywordSearch;
-
-/* 
-More specialized search options: 
-
-const healthCareResults = filteredResults.filter(e => e.description.includes("health"));
-
-const mentalHealthCareResults = filteredResults.filter(e => e.description.includes("mental health"));
-
-*/
